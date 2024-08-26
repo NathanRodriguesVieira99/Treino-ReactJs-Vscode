af39098ea17c3103cc634972a392ff7c2176f934
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-08-26 18:24:28 -0300

    esse projeto era sem o vite :/

--- estudos_react_vscode/src/App.js
+++ estudos_react_vscode/src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
-	const [nathan, setNathan] = useState("");
+	const [nome, setNome] = useState("");
 
 	return (
 		<div className="App">
-			<h1>Olá {nathan}</h1>
-			<input type="text" onChange={(e) => setNathan(e.target.value)} />
+			<h1>Olá {nome}</h1>
+			<input type="text" onChange={(e) => setNome(e.target.value)} />
 		</div>
 	);
 }
